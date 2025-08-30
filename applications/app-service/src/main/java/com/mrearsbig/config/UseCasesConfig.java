24c7d79d1c3788a9d9d738576618e952aaf2686d
Alexander Viafara <mrearsbig@gmail.com>
2025-08-30 18:20:32 -0500

    feat: Initialize project structure with Clean Architecture
    - Added .gitignore file to exclude unnecessary files and directories.
    - Created README.md to provide an overview of the project and its architecture.
    - Implemented build.gradle for app-service module with necessary dependencies and tasks.
    - Developed MainApplication class to bootstrap the Spring Boot application.
    - Configured UseCasesConfig for component scanning of use case beans.
    - Set up application.yaml for Spring configuration, including server port and H2 console.
    - Configured log4j2.properties for logging settings.
    - Added .gitignore for test directory to exclude specific test files.
    - Created UseCasesConfigTest to validate the existence of use case beans.
    - Established root build.gradle with project dependencies and configurations.
    - Developed Dockerfile for containerization of the application.
    - Created build.gradle files for model and usecase domains.
    - Updated gradle.properties with additional Gradle configurations.
    - Added gradle wrapper files for consistent Gradle version management.
    - Implemented gradlew and gradlew.bat for executing Gradle tasks.
    - Configured lombok settings in lombok.config.
    - Established main.gradle for common project configurations and tasks.
    - Updated settings.gradle to define project structure and include modules.

---
+++ applications/app-service/src/main/java/com/mrearsbig/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.mrearsbig.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.mrearsbig.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
